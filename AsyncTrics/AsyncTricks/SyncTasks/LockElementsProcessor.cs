c0816f968ca05b823ebb0fac0e74880b4b2e7b40
kozlov.stepan <Stepan.Kozlov@infotecs.ru>
2024-10-11 18:18:36 +0300

    Elements processor base

---
+++ AsyncTrics/AsyncTricks/SyncTasks/LockElementsProcessor.cs
@@ -0,0 +1,19 @@
+namespace AsyncTricks.SyncTasks;
+
+public class LockElementsProcessor: ElementsProcessor
+{
+    public override Task ProcessElementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task AddElementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task GetElementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AsyncTrics/AsyncTricks/SyncTasks/ProcessElement.cs
@@ -0,0 +1,7 @@
+namespace AsyncTricks.SyncTasks;
+
+public class ProcessElement
+{
+    public string? Name { get; set; }
+    public Guid Id { get; set; }
+}
