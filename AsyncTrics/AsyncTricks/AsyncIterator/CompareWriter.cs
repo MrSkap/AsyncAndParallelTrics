abdc3fc7639de14e18af9fe912fe092dbf564123
kozlov.stepan <Stepan.Kozlov@infotecs.ru>
2024-10-11 15:35:21 +0300

    Sync enumerable and Parallel.ForEach example

--- AsyncTrics/AsyncTricks/AsyncIterator/CompareWriter.cs
+++ AsyncTrics/AsyncTricks/AsyncIterator/CompareWriter.cs
@@ -78,6 +78,36 @@ public class CompareWriter
         WriteResults(bag);
     }
 
+    /// <summary>
+    ///     Получить поток пар для сравнения и обработать их параллельно.
+    /// </summary>
+    /// <param name="pairsCount">Кол-во пар.</param>
+    /// <param name="min">Минимальное значение.</param>
+    /// <param name="max">Максимальное значение.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    public async Task GeneratePairsAndParallelCompareAsync(int pairsCount, int min, int max,
+        CancellationToken cancellationToken)
+    {
+        var foos = _dataGenerator.GetFoos(pairsCount, min, max);
+
+        Console.WriteLine("Start process of comparing pairs");
+
+        var options = new ParallelOptions
+        {
+            CancellationToken = cancellationToken,
+            TaskScheduler = TaskScheduler.Default,
+            MaxDegreeOfParallelism = 20
+        };
+        var bag = new ConcurrentBag<string>();
+        await Parallel.ForEachAsync(foos, options, (pair, _) =>
+        {
+            bag.Add($"Compare {pair.Number1} and {pair.Number2} \n Result: {pair.Line}");
+            return ValueTask.CompletedTask;
+        });
+
+        WriteResults(bag);
+    }
+
     private void WriteResults(IEnumerable<string> results)
     {
         foreach (var result in results) Console.WriteLine(result);
